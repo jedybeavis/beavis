6469ef3302935de342429d58e268a86d1739e534
Beavis (HTML) <jedybeavis@gmail.com>
2015-09-21 14:27:27 +0300

    Fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,87 +3,77 @@ module.exports = function(grunt) {
     // 1. Вся настройка находится здесь
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
-       
-
-        concat: {
-            // 2. Настройка для объединения файлов находится тут
-		    dist: {
-		        src: [
-		            'source/js/*.js', // Все JS в папке libs
-		        ],
-		        dest: 'js/main.js',
-		    }
-        },
-        stylus: {
-		  compile: {
-		    options: {
-		      'include css': false,
-		      'linenos':false,
-		      'paths': ['css/', 'source/css']
-		    },
-		    files: {
-		      'css/main.css': 'source/css/*.styl',
-		    }
-		  }
-		},
-	    sprite:{
-	      all: {
-	        src: 'images/sprite/*.png',
-	        dest: 'images/spritesheet.png',
-			destCss: 'source/css/common/sprites.styl',
-			imgPath: '../images/spritesheet.png',
-		    // cssOpts: {
-		    //   cssClass: function (item) {
-		    //     var prefix = short ? short + '-' : '';
-		    //     return '.' + prefix + item.name + ':before';
-		    //   }
-		    // },
-		  }
-		},
-
-		jade: {
-		  compile: {
-            options: {
-                client: false,
-                pretty: true
-            },
-            files: [ {
-              cwd: "source/",
-              src: "*.jade",
-              dest: "",
-              expand: true,
-              ext: ".html"
-			} ]
+		// Compile JS
+		concat: {
+		// 2. Настройка для объединения файлов находится тут
+			dist: {
+				src: ['source/js/*.js',],
+				dest: 'js/main.js',
 			}
 		},
-		 watch: {
+		// Compile CSS
+		stylus: {
+			compile: {
+				options: {
+					'include css': false,
+					'linenos':false,
+					'paths': ['css/', 'source/css']
+				},
+				files: {
+					'css/main.css': 'source/css/*.styl',
+				}
+			}
+		},
+		// Compile Image Sprites
+		sprite:{
+			all: {
+				src: 'images/sprite/*.png',
+				dest: 'images/spritesheet.png',
+				destCss: 'source/css/common/sprites.styl',
+				imgPath: '../images/spritesheet.png',
+			}
+		},
+		// Compile Jade
+		jade: {
+			compile: {
+				options: {
+					client: false,
+					pretty: true
+				},
+				files: [ {
+					cwd: "source/",
+					src: "*.jade",
+					dest: "",
+					expand: true,
+					ext: ".html"
+				} ]
+			}
+		},
+		watch: {
+			options: { livereload: true, },
 			css: {
-			    files: ['source/css/*.styl', 'source/css/blocks/*.styl'],
-			    tasks: ['stylus'],
-			    options: {
-			        spawn: false,
-			    }
+				files: ['source/css/*.styl', 'source/css/blocks/*.styl'],
+				tasks: ['stylus'],
+				options: {
+					spawn: false,
+				}
 			},
 			html: {
-			    files: ['source/*.jade', 'source/include_tpl/*.jade'],
-			    tasks: ['jade'],
-			    options: {
-			        spawn: false,
-			    }
+				files: ['source/*.jade', 'source/include_tpl/*.jade'],
+				tasks: ['jade'],
+				options: {
+					spawn: false,
+				}
 			},
 			js: {
-			    files: ['source/js/*.js', 'source/include_tpl/js/*.js'],
-			    tasks: ['concat'],
-			    options: {
-			        spawn: false,
-			    }
+				files: ['source/js/*.js', 'source/include_tpl/js/*.js'],
+				tasks: ['concat'],
+				options: {
+					spawn: false,
+				}
 			}
 		}
-
-
-
-    });
+	});
 
     // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
     grunt.loadNpmTasks('grunt-contrib-concat');
